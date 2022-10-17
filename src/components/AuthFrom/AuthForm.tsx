b53cf5f2d08382af8a09b2f5c63d7fd8003f9697
Roman Chechyotkin <romanchechyotkin@gmail.com>
2022-10-17 16:47:00 +0300

    react validation for email and password inputs added

--- src/components/AuthFrom/AuthForm.tsx
+++ src/components/AuthFrom/AuthForm.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import cls from './AuthForm.module.scss'
 import {login, registration} from "../../api/authClient";
 import {Auth, AuthFormProps} from "../../types/authTypes";
@@ -7,7 +7,19 @@ import {Link} from "react-router-dom";
 const AuthForm: FC<AuthFormProps> = ({type}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailDirty, setEmailDirty] = useState(false);
+    const [passwordDirty, setPasswordDirty] = useState(false);
+    const [emailError, setEmailError] = useState('нельзя оставлять пустым');
+    const [passwordError, setPasswordError] = useState('нельзя оставлять пустым');
+    const [formValid, setFormValid] = useState(false);
 
+    useEffect(() => {
+        if (emailError || passwordError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [emailError, passwordError]);
 
     const handleAuth = async () => {
         switch (type) {
@@ -24,6 +36,39 @@ const AuthForm: FC<AuthFormProps> = ({type}) => {
         }
     }
 
+    const blurHandle = (e: React.FocusEvent<HTMLInputElement>) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
+
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('некорректная почта')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 5 || e.target.value.length > 16) {
+            setPasswordError('пароль должен быть длиннее 5 и короче 16 символов')
+            if (!e.target.value) {
+                setPasswordError('нельзя оставлять пустым')
+            }
+        } else {
+            setPasswordError('')
+        }
+    }
+
     return (
         <div className={cls.container}>
             {type === 'registration' ?
@@ -32,14 +77,28 @@ const AuthForm: FC<AuthFormProps> = ({type}) => {
                 <h2>Login</h2>
             }
             <label className={cls.authLabel}>
-                email
-                <input className={cls.formControl} value={email} onChange={(e) => (setEmail(e.target.value))} type="text"/>
+                {(emailDirty && emailError) ? <div style={{color: "red"}}>{emailError}</div> : <>email</>}
+                <input
+                    name='email'
+                    className={cls.formControl}
+                    value={email}
+                    onBlur={e => blurHandle(e)}
+                    onChange={e => emailHandler(e)}
+                    type="text"
+                />
             </label>
             <label className={cls.authLabel}>
-                password
-                <input className={cls.formControl} value={password} onChange={(e) => (setPassword(e.target.value))} type="password"/>
+                {(passwordDirty && passwordError) ?  <div style={{color: "red"}}>{passwordError}</div> : <>password</>}
+                <input
+                    name='password'
+                    className={cls.formControl}
+                    value={password}
+                    onBlur={e => blurHandle(e)}
+                    onChange={e => passwordHandler(e)}
+                    type="password"
+                />
             </label>
-            <button className={cls.authBtn} onClick={handleAuth}>ok</button>
+            <button disabled={!formValid} className={cls.authBtn} onClick={handleAuth}>ok</button>
             {type === 'registration' ?
                 <p>есть аккаунт? <Link to={'/login'}>Войди</Link></p>
             :
